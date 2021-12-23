f956de783f2bcee291b6e51157090bbc4ffa78f0
ztSso <1627832980@qq.com>
2021-12-23 20:49:17 +0800

    在“master”提交

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+//库函数的使用，得包含特定的头文件
+//std    i    o
+//标准  输入  输出
+//写C语言代码，首先要写出主函数 头文件
+//主流写法
+//int -- 整型
 int main()
-{ char ch1,ch2;
-    ch1='a';ch2='B';
-    printf("ch1=%c,ch2=%c\n",ch1-32,ch2+32);
-    printf("ch1+200=%d\n",ch1+200);
-    printf("ch1+200=%c\n",ch1+200);
-    printf("ch2+256=%d\n",ch1+256);
-    printf("ch2+256=%c\n",ch1+256);
+{
+    printf("Hello world,this is a C progarm!\n");
+    //printf是C语言提供的库函数
+    // print 打印 printf按指定格式输出字符串
     return 0;
+    //int返回一个值 返回0
 }
+//void main()
+//主函数为void没有返回值
+/* void作用：
+（1）对函数返回的限定；
+（2）对函数参数的限定。*/
