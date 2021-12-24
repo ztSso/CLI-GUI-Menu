dd1031becf3bd21e7e0fd8eadddf297f3324fc67
ztSso <1627832980@qq.com>
2021-12-24 20:49:43 +0800

    sizeof

--- helloworld.c
+++ helloworld.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-//库函数的使用，得包含特定的头文件
-//std    i    o
-//标准  输入  输出
-//写C语言代码，首先要写出主函数 头文件
-//主流写法
-//int -- 整型
+//sizeof是用来计算一个类型的大小的
+//单位是**字节**
 int main()
 {
-    printf("Hello world,this is a C progarm!\n");
-    //printf是C语言提供的库函数
-    // print 打印 printf按指定格式输出字符串
+    printf("%d\n",sizeof(char));
+    /*char的类型大小：1个字节（BYTE）==8位（bit） 所以有256种变化.
+   1）有符号字符型（signed char）：存储范围为-128~127
+    2）无符号字符型（unsigned char):存储范围为0~255 */
+    printf("%d\n",sizeof(short));
+    printf("%d\n",sizeof(int));
+    printf("%d\n",sizeof(long));
+    printf("%d\n",sizeof(long long));
+    /*整形int --4个字节；长整形long int --4个字节；短整形short int --2个字节
+    c语言规定：sizeof(long)>=sizeof(int)*/
+    printf("%d\n",sizeof(float));
+    printf("%d\n",sizeof(double));
+    printf("%d\n",sizeof(long double));
     return 0;
-    //int返回一个值 返回0
 }
-//void main()
-//主函数为void没有返回值
-/* void作用：
-（1）对函数返回的限定；
-（2）对函数参数的限定。*/
