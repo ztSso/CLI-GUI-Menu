4ab70a88c28a44e7348b6464ee2bdd7ba197ccb6
ztSso <1627832980@qq.com>
2022-03-02 16:22:55 +0800

    数组

--- helloworld.c
+++ helloworld.c
@@ -3,14 +3,11 @@
 
 int main()
 {
-    const int LENGTH =10;
-    const int WIDTH = 5;
-    const int NEWLINE = '\n';
-    int area;
-
-    area= LENGTH * WIDTH;
-    printf("valur of area : %d",area);
-    printf("%c",NEWLINE);
+    char arr1[]="abc";//数组 字符串包含0 --- 'a' 'b' 'c' '\0'
+    char arr2[]={'a','b','c',0};//'\0'=0 指字符串的结束标志
+    //'a'-97 数据在计算机上存储时，存储于的是二进制 ASCII编码
+    printf("%s\n",arr1);
+    printf("%s\n",arr2);
     system("pause");
     return 0;
-}
+    }
