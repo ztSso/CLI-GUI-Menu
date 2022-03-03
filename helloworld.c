fd3a3118bc0ed15e0c2e87a8652665a243a26e48
ztSso <1627832980@qq.com>
2022-03-03 17:38:45 +0800

    求园周长，圆面积，圆球体积的程序

--- helloworld.c
+++ helloworld.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int num,a,b,c;//定义变量num,a,b,c
-    scanf("%d",&num);//输入num
-    a=num/100;//a等于num输入的三位整数的百位数。int为整型，所a的取值为整数
-    b=(num-100*a)/10;
-    c=num%10;
-    a=c*100+b*10+a;
-    printf("%d\n",a);
+    float r,l,s,v;
+    scanf("%f",&r);
+    l=3.14*r*2;
+    s=3.14*r*r;
+    v=4.0/3*3.14*r*r*r;
+    printf("圆周长：%f,圆面积：%f,圆球面积：%f\n",l,s,v);
     system("pause");
     return 0;
+
 }
