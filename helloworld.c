e5f4b6395766e2b069ed02a2b8670f18ed8c5c14
ztSso <1627832980@qq.com>
2022-03-03 17:12:11 +0800

    使三位整数逆向输出的程序

--- helloworld.c
+++ helloworld.c
@@ -3,12 +3,13 @@
 
 int main()
 {
-    int a,b,c;//int定义变量a,b,c
-    scanf("%d%d",&a,&b);//输入a,b
-    c=a;//使变量c=变量a
-    a=b;
-    b=c;
-    printf("a=%d,b=%d\n",a,b);//输出a,b
+    int num,a,b,c;//定义变量num,a,b,c
+    scanf("%d",&num);//输入num
+    a=num/100;//a等于num输入的三位整数的百位数。int为整型，所a的取值为整数
+    b=(num-100*a)/10;
+    c=num%10;
+    a=c*100+b*10+a;
+    printf("%d\n",a);
     system("pause");
     return 0;
-    }
+}
